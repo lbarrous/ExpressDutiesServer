314f5f659da2da75c40524b9d5621f7af3c47a9c
Alvaro Perez Garcia <apgarcia@MacBook-Pro-de-Alvaro.local>
2021-03-26 16:05:51 +0100

    Added completed property

--- src/models/Duty.interface.ts
+++ src/models/Duty.interface.ts
@@ -2,4 +2,5 @@ import { Document } from "mongoose";
 
 export interface IDuty extends Document {
   name: string;
+  completed: boolean;
 }
--- src/models/Duty.model.ts
+++ src/models/Duty.model.ts
@@ -3,6 +3,7 @@ import { model, Schema } from "mongoose";
 
 const DutySchema = new Schema({
   name: { type: String, required: [true, "Name field is required"] },
+  completed: { type: Boolean, required: [true, "Name field is required"] },
 });
 
 export const Duty = model<IDuty>("Duty", DutySchema);
